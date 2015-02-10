112b47796a84479eb0a834cc1b3bdf8e2d4f7d6b
macrowa <macrowa@outlook.com>
2015-02-10 11:02:06 +0800

    ll

---
+++ Waz/Waz.Web/Extensions/HelloWorld/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorld.Models;
+
+namespace HelloWorld.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            Post post = new Post()
+            {
+                Title = "Hello Extension!",
+                Content = "Mvc Extension!",
+                Comments = new List<string>() { "good!", "thx" }
+            };
+            return View(post);
+        }
+
+    }
+}
---
+++ Waz/Waz.Web/Extensions/HelloWorld/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorld.Models
+{
+    public class Post
+    {
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public List<string> Comments { get; set; }
+    }
+}
